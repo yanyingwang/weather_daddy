7985de71b50eeb8bafef89fb04994879d2778855
Yanying Wang <yanyingwang@users.noreply.github.com>
2016-10-26 21:44:40 +0800

    Update today.rb

--- lib/weather_daddy/today.rb
+++ lib/weather_daddy/today.rb
@@ -3,7 +3,7 @@ class WeatherDaddy
   class << self
 
     def today_html
-      Nokogiri::HTML(open("http://tianqi.2345.com/today-#{@area_id}.htm"))
+      Nokogiri::HTML(open("https://tianqi.2345.com/today-#{@area_id}.htm"))
     end
 
     def today_arr
