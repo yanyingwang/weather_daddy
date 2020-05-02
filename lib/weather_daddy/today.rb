d71092bf2ab72377d4416d2ad6579c8b5dee3ad6
Yanying Wang <yanyingwang@users.noreply.github.com>
2020-05-02 10:20:24 +0800

    Update today.rb

--- lib/weather_daddy/today.rb
+++ lib/weather_daddy/today.rb
@@ -21,7 +21,7 @@ class WeatherDaddy
     end
 
     def day_of_week_today
-      int_to_day_of_week Time.now.wday
+      int_to_day_of_week Time.now.getlocal("+08:00").wday
     end
 
     def deliver_today_normal
